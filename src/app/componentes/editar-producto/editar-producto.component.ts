0ce3631d7f19f831ae92579624c4e0043ac56cbf
unknown <analucygdl@gmail.com>
2022-04-05 21:16:14 -0500

    Receive data by GET

--- src/app/componentes/editar-producto/editar-producto.component.ts
+++ src/app/componentes/editar-producto/editar-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CrudService } from 'src/app/servicio/crud.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-producto',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-producto.component.css']
 })
 export class EditarProductoComponent implements OnInit {
+  elID:any;
 
-  constructor() { }
+  constructor(
+    private activeRoute:ActivatedRoute
+  ) { 
+    this.elID = this.activeRoute.snapshot.paramMap.get('id');
+    console.log(this.elID)
+  }
 
   ngOnInit(): void {
   }
